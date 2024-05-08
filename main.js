49ac660d0df63db55bebd1c54603eb5129528905
Jubair1996 <abujubair1996@gmail.com>
2024-05-08 16:48:21 +0600

    js file added

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-/*===================================Manu Bar Icon Toggle  ==================================*/ 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+/*=================================== Manu Bar Icon Toggle  ==================================*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('fa-x');
-    navbar.classList.toggle('active');
-}
-/*=====================================================================*/
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("fa-x");
+  navbar.classList.toggle("active");
+};
+/*=================================== Scroll Reveal  ==================================*/
+/*=================================== Typed JS  ==================================*/
+let typed = new Typed(".multiple-text", {
+  strings: [
+    "Front-End Developer",
+    "JavaScript Developer",
+    "ReactJs Developer",
+    "NextJs Developer",
+    "Responsive Designer",
+    "Web Designer",
+    "PSD To HTM Designer",
+    "Figma To HTML Designer",
+  ],
+  typeSpeed: 70,
+  backspeed: 70,
+  backDelay: 100,
+  loop: true,
+});
